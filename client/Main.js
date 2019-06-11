2d672cecf717f313884093611e16d6a7c62c375b
Beno Suriano <bjsuriano@gmail.com>
2019-06-11 12:42:10 -0600

    ALL albums view completed.

--- client/Main.js
+++ client/Main.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Sidebar from './Sidebar'
+import Albums from './Albums'
+import Player from './Player'
 
 export default class Main extends React.Component {
   render () {
     return (
       <div id='main' className='row container'>
-        {/* The music starts here! */}
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Body */}
+        <Albums />
+
+        {/* Player */}
+        <Player />
+
       </div>
     )
   }
